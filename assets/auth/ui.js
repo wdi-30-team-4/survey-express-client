c90d4147ac485b522a4fa73663c820336f486d8a
NathanFee <Nathan_Fee@yahoo.com>
2019-03-27 16:38:42 -0400

    Adds Auth folder and files
    - Adds auth folder from previous assignment
    - Removes code that was project specific

---
+++ assets/auth/ui.js
@@ -0,0 +1,110 @@
+'use strict'
+
+const store = require('../store.js')
+
+const signUpSuccess = () => {
+  $('#user-message').html('Sign-up successful, please sign in.')
+  $('form').trigger('reset')
+  $('#sign-up-form').addClass('hidden')
+  removeMessage()
+  showSignInForm()
+}
+
+const signUpFailure = () => {
+  $('#user-message').html('Error on Sign-up.')
+  $('form').trigger('reset')
+  removeMessage()
+}
+
+const signInSuccess = (responseData) => {
+  $('form').trigger('reset')
+  store.user = responseData.user
+  $('.sign-in-display').addClass('hidden')
+  $('.swap-form-display').addClass('hidden')
+}
+
+const signInFailure = () => {
+  $('#user-message').html('Error on Sign-in.')
+  $('form').trigger('reset')
+  removeMessage()
+}
+
+const changePasswordSuccess = () => {
+  $('#modal-alert-message').html('Password Change Successful!')
+  $('form').trigger('reset')
+  removeMessage()
+}
+
+const changePasswordFailure = () => {
+  $('#modal-alert-message').html('Error Changing Password. Please try again.')
+  $('form').trigger('reset')
+  removeMessage()
+}
+
+const signOutSuccess = () => {
+  $('#user-message').html('Successfully Signed-Out!')
+  $('form').trigger('reset')
+  $('.swap-form-display').removeClass('hidden')
+  store.user = {}
+  showSignUpForm()
+  removeMessage()
+}
+
+const signOutFailure = () => {
+  $('#user-message').html('Error Signing Out!')
+  $('form').trigger('reset')
+  removeMessage()
+}
+
+const showSignInForm = function () {
+  $('.sign-up-display').addClass('hidden')
+  $('.sign-in-display').removeClass('hidden')
+  $('.sign-in-button').addClass('hidden')
+  $('.sign-up-button').removeClass('hidden')
+  $('form').trigger('reset')
+  $('.swap-form-message').html('Need an account?')
+}
+
+const showSignUpForm = function () {
+  $('.sign-in-display').addClass('hidden')
+  $('.sign-up-display').removeClass('hidden')
+  $('.sign-up-button').addClass('hidden')
+  $('.sign-in-button').removeClass('hidden')
+  $('form').trigger('reset')
+  $('.swap-form-message').html('Already have an account?')
+}
+
+const closePasswordModal = function () {
+  $('#changePasswordModal').modal('toggle')
+  // wait until modal fades before reseting forms
+  setTimeout(() => {
+    $('#change-password-form').trigger('reset')
+  }, 1500)
+}
+
+let timeOut = null
+
+const stopTimeout = function () {
+  clearTimeout(timeOut)
+}
+
+const removeMessage = function () {
+  stopTimeout()
+  timeOut = setTimeout(() => {
+    $('#user-message').html('')
+  }, 3500)
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure,
+  showSignInForm,
+  showSignUpForm,
+  closePasswordModal
+}
