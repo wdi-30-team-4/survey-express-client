5f154d245c4ee853eebd2d6d910bfe7c82d58cee
NathanFee <Nathan_Fee@yahoo.com>
2019-03-28 14:19:06 -0400

    Adds handlebars to get Surveys
    - Adds feature to display all surveys when get surveys button is
    clicked.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,8 +6,10 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 const authEvents = require('./auth/events.js')
+const surveyEvents = require('./surveys/events.js')
 
 $(() => {
+  $('#getSurveysButton').on('click', surveyEvents.onGetSurveys)
   // authorization
   $('#sign-up-form').on('submit', authEvents.onSignUp)
   $('#sign-in-form').on('submit', authEvents.onSignIn)
