bf92277d00cfc1f0ce55318a337d4086055dd440
NathanFee <Nathan_Fee@yahoo.com>
2019-03-28 07:22:36 -0400

    Links auth files with project
    - Adds html elements for auth
    - Adds event listeners to app.js
    - Links project to production site

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,17 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events.js')
 
 $(() => {
-  // your JS code goes here
+  // authorization
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#dropdown-sign-out-link').on('click', authEvents.onSignOut)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  // Show sign-in-form
+  $('.sign-in-button').on('click', authEvents.onSignInButton)
+  // Show sign-up-form
+  $('.sign-up-button').on('click', authEvents.onSignUpButton)
+  $('.close-password-modal').on('click', authEvents.onClosePasswordModal)
 })
