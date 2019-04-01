6c6b5cb2842348af1f6d84b063362e675d26f5ce
LucasLombardo <lucasalombardo@gmail.com>
2019-04-01 09:56:32 -0400

    Remove slash from deployed backend

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://polar-woodland-60280.herokuapp.com/',
+  production: 'https://polar-woodland-60280.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
